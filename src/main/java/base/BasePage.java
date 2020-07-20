302374589f0a43a45972dec4ae475e06e91bc964
Carolina O <carolina.n.olguin@gmail.com>
2020-07-20 02:48:28 -0300

    removed unused methods

--- src/main/java/base/BasePage.java
+++ src/main/java/base/BasePage.java
@@ -3,7 +3,6 @@ package base;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasePage {
@@ -15,14 +14,7 @@ public class BasePage {
         this.driver = driver;
         wait = new WebDriverWait(driver, 70);
     }
-
-
-    public void selectDropDown(By locator, String element) {
-        wait.until(ExpectedConditions.presenceOfElementLocated(locator));
-        Select select = new Select(driver.findElement(locator));
-        select.selectByVisibleText(element);
-    }
-
+    
 
     public void sleepSecond(int value) {
         try {
@@ -43,11 +35,6 @@ public class BasePage {
         driver.findElement(locator).click();
     }
 
-    public String getText(By locator) {
-        wait.until(ExpectedConditions.presenceOfElementLocated(locator));
-        return driver.findElement(locator).getText();
-    }
-
     public void waitForElementVisibility(By locator) {
         wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
@@ -62,10 +49,6 @@ public class BasePage {
 
     public boolean waitForUrlContains(String dataUrl) {
       return   wait.until(ExpectedConditions.urlContains(dataUrl));
-    }
-
-    public void clickOnLinkText(String link) {
-        driver.findElement(By.linkText(link)).click();
     }
 
 }
